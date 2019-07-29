6a8288367ffd69e1c0756d1fe6e53edb46f9c93a
Vicki Lee <vickihlee@users.noreply.github.com>
2019-07-28 19:54:47 -0700

    Add files via upload

---
+++ Exercise1-AreaOfARoom.py
@@ -0,0 +1,11 @@
+#Vicki Lee CS110A 6/30/19 HW2
+
+#Exercise 1: Area of a Room
+
+#Input of width and length of a room
+width = float(input('Enter width of room (in feet):\n'))
+length = float(input('Enter length of room (in feet):\n'))
+
+#Area of room
+area = width * length
+print'Area of room is:', '%.02f' % area, 'feet.'
